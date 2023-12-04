4e0572e185f935a707b8c6d7538d45875277d09b
MonthlyScooters <152426158+MonthlyScooters@users.noreply.github.com>
2023-12-04 21:21:29 +0700

    Update message_decoder.py

--- message_decoder.py
+++ message_decoder.py
@@ -1,31 +1,22 @@
 def decode_message(encoded_message):
-    """
-    Decodes a JSON-encoded message into a message object.
-
-    Args:
-        encoded_message (str): JSON-encoded message string
-
-    Returns:
-        Message: Decoded message object
-    """
-    message = json.loads(encoded_message)
-
-    message_type = message["message_type"]
-    sender_id = message["sender"]
-    recipient_id = message["recipient"]
-    payload = message["payload"]
-
-    return Message(message_type, sender_id, recipient_id, payload)
-
-def decode_message(encoded_message):
     # Decode the JSON message
     message = json.loads(encoded_message)
 
     # Check for missing or invalid fields
-    ...
+    required_fields = ["message_type", "sender", "recipient", "payload"]
+    for field in required_fields:
+        if field not in message:
+            raise ValueError(f"Missing required field: {field}")
 
-    # Raise an INVALID_MESSAGE_FORMAT error if invalid
-    ...
+    # Check if message type is valid
+    if message["message_type"] not in VALID_MESSAGE_TYPES:
+        raise ValueError(f"Invalid message type: {message['message_type']}")
+
+    # Check if sender ID and recipient ID are valid
+    if not validate_id(message["sender"]):
+        raise ValueError(f"Invalid sender ID: {message['sender']}")
+    if not validate_id(message["recipient"]):
+        raise ValueError(f"Invalid recipient ID: {message['recipient']}")
 
     # Construct the Message object if valid
     return Message(message_type, sender_id, recipient_id, payload)

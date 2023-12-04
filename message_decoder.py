f85cdfd6b715ffc7fe2672f7d632a7e38a11afdd
MonthlyScooters <152426158+MonthlyScooters@users.noreply.github.com>
2023-12-04 20:49:02 +0700

    Update message_decoder.py

--- message_decoder.py
+++ message_decoder.py
@@ -16,3 +16,16 @@ def decode_message(encoded_message):
     payload = message["payload"]
 
     return Message(message_type, sender_id, recipient_id, payload)
+
+def decode_message(encoded_message):
+    # Decode the JSON message
+    message = json.loads(encoded_message)
+
+    # Check for missing or invalid fields
+    ...
+
+    # Raise an INVALID_MESSAGE_FORMAT error if invalid
+    ...
+
+    # Construct the Message object if valid
+    return Message(message_type, sender_id, recipient_id, payload)

bc7b97f2cbafe6f4285560f9082a0b9d82ad551c
MonthlyScooters <152426158+MonthlyScooters@users.noreply.github.com>
2023-12-04 21:23:53 +0700

    Update message_router.py

--- message_router.py
+++ message_router.py
@@ -3,7 +3,12 @@ class MessageRouter:
         self.topic_subscriptions = {}
 
     def route_message(self, encoded_message):
-        message = decode_message(encoded_message)
+        try:
+            message = decode_message(encoded_message)
+        except ValueError as e:
+            # Handle invalid message format
+            print(f"Invalid message format: {e}")
+            return
 
         if message.message_type == "request":
             # Route request messages directly to the specified recipient
@@ -15,11 +20,10 @@ class MessageRouter:
             request_id = message.payload["request_id"]
             sender_id = message.payload["sender_id"]
 
-            if request_id not in self.topic_subscriptions:
-                # No route found for response, discard the message
+            recipients = self.topic_subscriptions.get(request_id, [])
+            if not recipients:
+                # No subscribers for this request, discard the message
                 return
-
-            recipients = self.topic_subscriptions[request_id]
 
             for recipient in recipients:
                 self.deliver_message(message, recipient)
@@ -28,11 +32,10 @@ class MessageRouter:
             # Broadcast information messages to all subscribed language models
             topic = message.payload["information_type"]
 
-            if topic not in self.topic_subscriptions:
+            recipients = self.topic_subscriptions.get(topic, [])
+            if not recipients:
                 # No subscribers for this topic, discard the message
                 return
-
-            recipients = self.topic_subscriptions[topic]
 
             for recipient in recipients:
                 self.deliver_message(message, recipient)
@@ -41,44 +44,3 @@ class MessageRouter:
             # Not applicable for routing, simply discard
             return
 
-    def deliver_message(self, encoded_message, recipient_id):
-        # Implement actual message delivery mechanism (e.g., network communication)
-        print(f"Delivering message to {recipient_id}")
-
-class MessageRouter:
-    # ... existing routing logic ...
-
-    def deliver_message(self, encoded_message, recipient_id):
-        # Implement actual message delivery mechanism
-        ...
-
-        try:
-            # Attempt message delivery
-            ...
-        except Exception as e:
-            # Handle delivery failure
-            ...
-
-            # Check error code and generate appropriate error message
-            ...
-
-            # Implement retry mechanism with backoff algorithm
-            ...
-
-            # Log the error and discard the message if retry limit reached
-            ...
-class MessageRouter:
-    # ... existing routing logic ...
-
-    def route_message(self, encoded_message):
-        # ... existing routing logic ...
-
-        # Check if the recipient is unavailable
-        is_unavailable = self.check_unavailability(recipient_id)
-
-        if is_unavailable:
-            # Generate appropriate error message
-            ...
-
-            # Discard the message
-            ...

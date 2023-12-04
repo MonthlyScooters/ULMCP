3cd0d121443746cb92f42e7540be96fa919d8aa9
MonthlyScooters <152426158+MonthlyScooters@users.noreply.github.com>
2023-12-04 20:07:41 +0700

    Create message_encoder.py

---
+++ message_encoder.py
@@ -0,0 +1,18 @@
+def encode_message(message_type, sender_id, recipient_id, payload):
+    """
+    Encodes a message into JSON format.
+
+    Args:
+        message_type (str): Type of the message (e.g., request, response, information)
+        sender_id (str): Identifier of the sending language model
+        recipient_id (str): Identifier of the receiving language model (optional for broadcast messages)
+        payload (dict): Data structure carrying the message content
+    """
+    message = {
+        "message_type": message_type,
+        "sender": sender_id,
+        "recipient": recipient_id,
+        "payload": payload
+    }
+
+    return json.dumps(message)
